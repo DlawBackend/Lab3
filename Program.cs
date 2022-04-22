d223bf6d350625d5a13b231c8f461e379e15724c
wellahealthschool github <dlaweyenghe@gmil.com>
2022-04-23 00:51:28 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+        
+             
+            if(number % 2 == 0) {
+                Console.WriteLine("Not Prime Number");
+            } else {
+                Console.WriteLine("Prime number");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
